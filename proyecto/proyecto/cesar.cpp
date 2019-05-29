46ab3d53f0f866dc47d5ce2b95509fda96edbe75
Cesar Torres <cesarf9t@hotmail.com>
2019-05-29 14:22:29 -0500

    sfdsfds

---
+++ proyecto/proyecto/cesar.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	cout << "Hola papu" << endl;
+
+	for (int i = 0;i < 20;i++)
+	{
+		cout << i*10 << endl;
+	}
+
+	system("pause");
+	return 0;
+}
---
+++ proyecto/proyecto/cesar2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	cout << "Hola papu 2" << endl;
+
+	
+	system("pause");
+	return 0;
+}
